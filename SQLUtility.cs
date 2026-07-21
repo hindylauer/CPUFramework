80e0db1345c94e3536d6b279c3ea05dc7facd3fc
hindylauer <hindylauer@gmail.com>
2026-07-21 14:10:45 -0500

    #48

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -31,6 +31,44 @@ namespace CPUFramework
             GetDataTable(sqlstatement);
         }
 
+        public static int GetFirstColumnFirstRowValue(string sql)
+        {
+            int n = 0;
+
+            DataTable dt = GetDataTable(sql);
+            if(dt.Rows.Count > 0 && dt.Columns.Count > 0)
+            {
+                if(dt.Rows[0][0] != DBNull.Value)
+                {
+                    
+                    int.TryParse(dt.Rows[0][0].ToString(), out n);
+                }
+                
+            }
+            return n;
+        }   
+
+        public static string GetFirstColumnFirstRowString(string sql)
+        {
+            DataTable dt = GetDataTable(sql);
+
+            if(dt.Rows.Count == 0)
+            {
+                return "";
+            }
+
+            return dt.Rows[0][0].ToString();
+
+        }
+
+        public static DateTime GetFirstColumnFirstRowsDateTime(string sql)
+        {
+            DataTable dt = GetDataTable(sql);
+
+            return Convert.ToDateTime(dt.Rows[0][0]);
+        }
+
+
         private static void SetAllColumnsAllowNulls(DataTable dt)
         {
             foreach(DataColumn c in dt.Columns)
