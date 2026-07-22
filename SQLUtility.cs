0924ec3381bc666fa1fe7a0826a55b15d9350b80
hindylauer <hindylauer@gmail.com>
2026-07-22 12:29:05 -0500

    #50

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -8,22 +8,39 @@ namespace CPUFramework
     public class SQLUtility
     {
         public static string ConnectionString = "";
-        public static DataTable GetDataTable(string sqlstatement) //- take a SQL statement and return a data table
+
+        public static SqlCommand GetSqlCommand(String sprocname)
         {
-            Debug.Print(sqlstatement);
+            SqlCommand cmd;
+            using (SqlConnection conn = new SqlConnection(SQLUtility.ConnectionString))
+            {
+                cmd = new SqlCommand(sprocname, conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                conn.Open();
+                SqlCommandBuilder.DeriveParameters(cmd);
+            }
+            return cmd;
+        }
+
+        public static DataTable GetDataTable(SqlCommand cmd)
+        {
+            Debug.Print("-----" + Environment.NewLine + cmd.CommandText);
             DataTable dt = new();
-            SqlConnection conn = new();
-            conn.ConnectionString = ConnectionString;
-            conn.Open();
-
-
-            var cmd = new SqlCommand();
-            cmd.Connection = conn;
-            cmd.CommandText = sqlstatement;
-            var dr = cmd.ExecuteReader();
-            dt.Load(dr);
+            using (SqlConnection conn = new SqlConnection(SQLUtility.ConnectionString))
+            {
+                conn.Open();
+                cmd.Connection = conn;
+                SqlDataReader dr = cmd.ExecuteReader();
+                dt.Load(dr);
+            }
             SetAllColumnsAllowNulls(dt);
             return dt;
+        }
+
+
+        public static DataTable GetDataTable(string sqlstatement) //- take a SQL statement and return a data table
+        {
+            return GetDataTable(new SqlCommand(sqlstatement));
         }
 
         public static void ExecuteSql(string sqlstatement)
